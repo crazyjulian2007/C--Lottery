8218a350cd36a408e9fde3c6461c4b3d38f03371
Jeremy Huang <crazyjulian2007@gmail.com>
2018-08-15 21:38:42 +0800

    C#  WebForm LotterySystem

---
+++ HomeWork5/HomeWork5/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace HomeWork5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFinalNumber = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btAgain = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNumChosed = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btRule = new System.Windows.Forms.Button();
+            this.btMoney = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(211, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "雙贏彩投注單";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.label2.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(735, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "中獎號碼";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblFinalNumber
+            // 
+            this.lblFinalNumber.AutoEllipsis = true;
+            this.lblFinalNumber.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.lblFinalNumber.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblFinalNumber.Location = new System.Drawing.Point(737, 116);
+            this.lblFinalNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFinalNumber.Name = "lblFinalNumber";
+            this.lblFinalNumber.Size = new System.Drawing.Size(216, 497);
+            this.lblFinalNumber.TabIndex = 2;
+            // 
+            // btStart
+            // 
+            this.btStart.Font = new System.Drawing.Font("標楷體", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btStart.Location = new System.Drawing.Point(580, 154);
+            this.btStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(108, 64);
+            this.btStart.TabIndex = 3;
+            this.btStart.Text = "開獎";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btAgain
+            // 
+            this.btAgain.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btAgain.Location = new System.Drawing.Point(580, 241);
+            this.btAgain.Margin = new System.Windows.Forms.Padding(2);
+            this.btAgain.Name = "btAgain";
+            this.btAgain.Size = new System.Drawing.Size(108, 64);
+            this.btAgain.TabIndex = 4;
+            this.btAgain.Text = "再玩一次";
+            this.btAgain.UseVisualStyleBackColor = true;
+            this.btAgain.Click += new System.EventHandler(this.btAgain_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Tomato;
+            this.label3.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(316, 59);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "投注號碼";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblNumChosed
+            // 
+            this.lblNumChosed.BackColor = System.Drawing.Color.Tomato;
+            this.lblNumChosed.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblNumChosed.Location = new System.Drawing.Point(316, 116);
+            this.lblNumChosed.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumChosed.Name = "lblNumChosed";
+            this.lblNumChosed.Size = new System.Drawing.Size(217, 497);
+            this.lblNumChosed.TabIndex = 7;
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.SlateBlue;
+            this.label14.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label14.ForeColor = System.Drawing.SystemColors.Control;
+            this.label14.Location = new System.Drawing.Point(571, 116);
+            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(123, 23);
+            this.label14.TabIndex = 18;
+            this.label14.Text = "請任選12個號碼";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btRule
+            // 
+            this.btRule.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btRule.Location = new System.Drawing.Point(580, 333);
+            this.btRule.Margin = new System.Windows.Forms.Padding(2);
+            this.btRule.Name = "btRule";
+            this.btRule.Size = new System.Drawing.Size(108, 64);
+            this.btRule.TabIndex = 19;
+            this.btRule.Text = "遊戲規則";
+            this.btRule.UseVisualStyleBackColor = true;
+            this.btRule.Click += new System.EventHandler(this.btRule_Click);
+            // 
+            // btMoney
+            // 
+            this.btMoney.Font = new System.Drawing.Font("標楷體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btMoney.Location = new System.Drawing.Point(580, 423);
+            this.btMoney.Margin = new System.Windows.Forms.Padding(2);
+            this.btMoney.Name = "btMoney";
+            this.btMoney.Size = new System.Drawing.Size(108, 64);
+            this.btMoney.TabIndex = 20;
+            this.btMoney.Text = "獎金";
+            this.btMoney.UseVisualStyleBackColor = true;
+            this.btMoney.Click += new System.EventHandler(this.btMoney_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(35, 59);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 39);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "選號區";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1279, 703);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btMoney);
+            this.Controls.Add(this.btRule);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lblNumChosed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btAgain);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lblFinalNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "雙贏彩投注";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFinalNumber;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btAgain;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNumChosed;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btRule;
+        private System.Windows.Forms.Button btMoney;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HomeWork5/HomeWork5/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork5
+{
+    public partial class Form1 : Form
+    {
+        Random ranx = new Random(Guid.NewGuid().GetHashCode());
+        Random rany = new Random(Guid.NewGuid().GetHashCode());
+        List<int> lottoNum;
+        List<int> tempLottoNum;
+        List<int> tempNumArray;
+        List<int> NumMessageList;
+        List<int> compareNum;
+        List<Button> myDButtonList;
+        List<Button> chosedButton;
+        Button myButton ;
+        int choseNum = 12;
+        string strNumMessage;
+        bool ifShow = false;
+        bool ifChose = false;
+        System.Timers.Timer t = new System.Timers.Timer(2000); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myDButtonList = new List<Button>();
+            NumMessageList = new List<int>();
+            chosedButton = new List<Button>(); 
+            CreatButton(6, 4);
+            lottoNum = new List<int>(Enumerable.Range(1, 24));
+            tempLottoNum = new List<int>();
+            tempNumArray = new List<int>();
+            compareNum = new List<int>();
+
+            timer1.Enabled = true;
+            t.Elapsed += new System.Timers.ElapsedEventHandler(shine); 
+            t.AutoReset = false;
+            t.SynchronizingObject = this;
+        }
+
+        void shine(object sender, System.Timers.ElapsedEventArgs e )
+        {
+            try
+            {
+                if (NumMessageList.Count == choseNum)
+                {
+                    timer1.Stop();
+                    lblFinalNumber.Text = "";
+                    lblNumChosed.Text = "";
+                    tempLottoNum = lottoNum.ToList();
+                    Lotto(tempLottoNum);
+                    CompareValue(tempLottoNum);
+                }
+                else
+                {
+                    //MessageBox.Show("Please select 12 Numbers");
+                    Again();
+                }
+            }
+            catch (Exception error)
+            {
+                //MessageBox.Show(error.ToString());
+            }
+        }
+
+        void dButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myButton = (Button)sender;
+                myButton.BackColor = Color.Red;
+
+                if (ifChose == true)
+                {
+                    myButton.BackColor = Color.Pink;
+                    ifChose = false;
+                    for (int i = 0; i <= NumMessageList.Count; i++)
+                    {
+                        NumMessageList.Remove(Convert.ToInt32(myButton.Name));
+                        chosedButton.Remove(myButton);
+                    }
+                }
+                else
+                {
+                    ifChose = true;
+                    myButton.BackColor = Color.Red;
+                    NumMessageList.Add(Convert.ToInt32(myButton.Name));
+                    chosedButton.Add(myButton);
+                }
+                
+                if (NumMessageList.Count == choseNum)
+                {
+                    btStop();
+                }
+            }
+            catch (Exception error)
+            {
+                //MessageBox.Show(error.ToString());
+            }
+        }
+
+        void CreatButton(int intCount1, int intCount2)
+        {
+            int btName = 1;
+            int btText = 1;
+            for (int i = 0; i < intCount1; i++)
+            {
+                for (int j = 0; j < intCount2; j++)
+                {
+                    Button dButton = new Button();
+                    dButton.BackColor = Color.Pink;
+                    dButton.ForeColor = Color.Blue;
+                    dButton.Text = btText++.ToString(); 
+                    dButton.Name = btName++.ToString();
+                    dButton.Font = new Font("微軟正黑體", 16);
+                    dButton.Size = new Size(60, 40);
+                    dButton.Location = new Point(20 + 62 * j, 120 + 42 * i);
+                    dButton.Click += new EventHandler(dButton_Click);
+                    //dButton.Click -= new EventHandler(dButton_Click);
+
+                    Controls.Add(dButton);
+                    myDButtonList.Add(dButton);
+                }
+            }
+        }
+
+        void btStop() 
+        {
+            try
+            {
+                for (int i = 0; i < lottoNum.Count; i++)
+                {
+                    myDButtonList[i].Enabled = false;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        void btReStart() 
+        {
+
+            try
+            {
+                for (int i = 0; i < lottoNum.Count; i++)
+                {
+                    myDButtonList[i].Enabled = true;
+                    myDButtonList[i].BackColor = Color.Pink;
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            lblFinalNumber.Text = "";
+            
+            if (NumMessageList.Count == choseNum)
+            {
+                timer1.Start();
+
+                t.Enabled = true;
+                t.Start();
+
+                if (ifShow == true)
+                {
+                    ifShow = false;
+                }
+                else
+                {
+                    ifShow = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select 12 Numbers");
+                Again();
+            }
+        }
+
+        void Lotto(List<int> myLottoNum)
+        {
+            int maxNum = 24;
+            int randomNum = 0;
+            string strMessage = "";
+            
+            try
+            {
+                //LottoNum = LottoNum.OrderBy(num=>ranx.Next()).ToList<int>();
+                //Console.WriteLine(LottoNum[i].ToString()); random number won't repeat
+                for (int i = 0; i < choseNum; i++)
+                {
+                    randomNum = ranx.Next(maxNum-=1);
+                    myLottoNum.RemoveAt(randomNum);
+                }
+                myLottoNum.Sort();
+                foreach (int item in myLottoNum)
+                {
+                    strMessage += item+"\n";
+                }
+                lblFinalNumber.Text = strMessage;
+                btStart.Enabled = false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void btAgain_Click(object sender, EventArgs e)
+        {
+            Again();
+        }
+
+        void Again()
+        {
+            btReStart();
+
+            lblFinalNumber.Text = "";
+
+            btStart.Enabled = true;
+
+            tempNumArray.Clear();
+
+            lblNumChosed.Text = "";
+
+            NumMessageList.Clear();
+
+            strNumMessage = "";
+
+            ifShow = false;
+
+            compareNum.Clear();
+
+            chosedButton.Clear();
+        }
+
+        void CompareValue(List<int> myLottoNum)
+        {
+            try
+            {
+                NumMessageList.Sort();
+                foreach (int item in NumMessageList)
+                {
+                    strNumMessage += item.ToString() + "\n";
+                }
+                lblNumChosed.Text = strNumMessage;
+
+                if (NumMessageList.Count == choseNum)
+                {
+                    for (int i = 0; i < choseNum; i++)
+                    {
+                        for (int j = 0; j < choseNum; j++)
+                        {
+                            if (NumMessageList[i] == myLottoNum[j])
+                            {
+                                compareNum.Add(NumMessageList[i]);
+                            }
+                        }
+                    }
+                    for (int i = 0; i < myDButtonList.Count; i++)
+                    {
+                        for (int j = 0; j < myLottoNum.Count; j++)
+                        {
+                            if (myDButtonList[i].Name.ToString() == myLottoNum[j].ToString())
+                            {
+                                myDButtonList[i].BackColor = Color.MediumSlateBlue;
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 12; i++)
+                    {
+                        chosedButton[i].BackColor = Color.Tomato;
+                        for (int j = 0; j < compareNum.Count; j++)
+                        {
+                            if (chosedButton[i].Name.ToString() == compareNum[j].ToString())
+                            {
+                                chosedButton[i].BackColor = Color.Cyan;
+                            }
+                            
+                        }
+                    }
+                    if ((compareNum.Count == 12) || (compareNum.Count == 0))
+                    {
+                        MessageBox.Show( compareNum.Count + " Numbers are qualified, You Win the First Prize, You Get NT$15,000,000. ");
+                    }
+                    else if ((compareNum.Count == 11) || (compareNum.Count == 1))
+                    {
+                        MessageBox.Show(compareNum.Count + " Numbers are qualified, You Win the Second Prize, You Get NT$100,000. ");
+                    }
+                    else if ((compareNum.Count == 10) || (compareNum.Count == 2))
+                    {
+                        MessageBox.Show(compareNum.Count + " Numbers are qualified, You Win the third Prize, You Get NT$500. ");
+                    }
+                    else if ((compareNum.Count == 9) || (compareNum.Count == 3))
+                    {
+                        MessageBox.Show(compareNum.Count + " Numbers are qualified, You Win the Forth Prize, You Get NT$100. ");
+                    }
+                    else
+                    {
+                        MessageBox.Show(compareNum.Count + " Numbers are qualified, Sorry, You Get Nothing. ");
+                    }
+                    
+                    Again();
+                }
+                else
+                {
+                    MessageBox.Show("Please select 12 Numbers");
+                    Again();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        private void btRule_Click(object sender, EventArgs e)
+        {
+            lblFinalNumber.Text = "";
+            lblFinalNumber.Text = "雙贏彩是一種樂透型遊戲，您必須從01~24的號碼中任選12個號碼進行投注。" +
+                "開獎時，開獎單位將隨機開出12個號碼，這一組號碼就是該期雙贏彩的中獎號碼。\n" +
+                "頭獎:對中當期12個獎號或對中當期0個獎號。\n" +
+                "二獎:對中當期任11個獎號或對中任1個獎號。\n" +
+                "三獎:對中當期任10個獎號或對中任2個獎號。\n" +
+                "四獎:對中當期任9個獎號或對中任3個獎號。";
+        }
+
+        private void btMoney_Click(object sender, EventArgs e)
+        {
+            string strFirstPrize = "頭獎 : $15,000,000";
+            string strSecondPrize = "二獎 : $100,000";
+            string strThirdPrize = "三獎 : $500";
+            string strForthPrize = "四獎 : $100";
+            string strMessage = "";
+
+            lblNumChosed.Text = "";
+            strMessage = string.Format("{0},\n{1},\n{2},\n{3}.",
+                strFirstPrize,strSecondPrize,strThirdPrize,strForthPrize);
+            lblNumChosed.Text = strMessage;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int randomNumx = ranx.Next(24);
+            int randomNumy = rany.Next(24);
+            string strMessage = "";
+
+            try
+            {
+                if (ifShow == true)
+                {
+                    for (int i = 0; i < 12; i++)
+                    {
+                        chosedButton[ranx.Next(12)].BackColor = Color.PowderBlue;
+                        chosedButton[rany.Next(12)].BackColor = Color.Purple;
+                    }
+                    for (int i = 0; i < 12; i++)
+                    {
+                        strMessage += ranx.Next(1,24).ToString() + "\n";
+                        lblNumChosed.Text = strMessage;
+                        lblFinalNumber.Text = strMessage;
+                    }
+
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+            
+        }
+    }
+}
